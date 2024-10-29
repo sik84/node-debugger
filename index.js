9f02b0c27eb2123bfdd37d0bc267b2b073d6ca4e
sik84 <stefan.sikiric@gmail.com>
2024-10-29 16:36:53 +0100

    Hausaufgabe erledigt und abgelegt.

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
 // index.js
-var items = [1, "2", 3, "four", 5, null, undefined, 6]
-const results = []
+var items = [1, "2", 3, "four", 5, null, undefined, 6];
+const results = [];
 
-items.foreach((item) => {
-    if(typeof item === 'number')
-        if(item % 2 === 0) results.push(item* 2)
-
-    else if(item === null)
-    console.log("Found a null item")
-
-    else 
-    {
-        result.push(parseInt(item))
+items.forEach((item) => {
+  if(typeof item === 'number') {
+    if(item % 2 === 0) results.push(item* 2);
+  } else if(item === null) {
+    console.log("Found a null item");
+  } else {
+    results.push(parseInt(item));
     }
 });
 
-console.log("Original items:", items)
-console.log("Filtered and transformed items:", results)
+console.log("Original items:", items);
+console.log("Filtered and transformed items:", results);
